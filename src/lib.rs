0152b3b81cbc417c473a35619082ff91f711a4d7
Kafui Mortey <morteykafui@gmail.com>
2024-06-20 18:55:02 +0000

    add iterator to case_insensitive method

--- src/lib.rs
+++ src/lib.rs
@@ -72,15 +72,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_uppercase().contains(&query) {
-            results.push(line);
-        }
-    }
+    contents
+        .lines()
+        .filter(|line| line.contains(&query))
+        .collect()
 
-    results
+    // for line in contents.lines() {
+    //     if line.to_uppercase().contains(&query) {
+    //         results.push(line);
+    //     }
+    // }
+
+    // results
 }
 
 #[cfg(test)]
